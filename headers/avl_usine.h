94073d1f8bd7b7fdac6f4e944592b912e2f6a72e
Ilyes1810 <mdk95130@gmail.com>
2025-12-05 16:20:43 +0100

    Add header guards and max utility function
    Added header guards and utility function max()

--- headers/avl_usine.h
+++ headers/avl_usine.h
@@ -1,1 +1,10 @@
+#ifndef AVL_USINE_H
+#define AVL_USINE_H
 
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "types.h" // Contient la définition de AVL_Node_Usine_t et Troncon_CSV_t
+
+// Fonction utilitaire max() si non déjà définie (nécessaire pour les hauteurs)
+#define max(a, b) ((a > b) ? a : b)
