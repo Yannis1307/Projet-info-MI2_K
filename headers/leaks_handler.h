a626977d0cc1615d152126f5a002827ace2d0d2e
mehdi9513 <mehdi.abderrahmane95130@gmail.com>
2025-12-14 03:07:25 +0100

    Create leaks_handler.h

---
+++ headers/leaks_handler.h
@@ -0,0 +1,11 @@
+#ifndef LEAKS_HANDLER_H
+#define LEAKS_HANDLER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "types.h"
+
+
+int handle_leaks_data(const char *target_factory_id, const char *input_source);
+
+#endif // LEAKS_HANDLER_H
