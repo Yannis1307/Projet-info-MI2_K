34c3b1b34d4855fec15abef2a7088ae2dcdc9e8d
mehdi9513 <mehdi.abderrahmane95130@gmail.com>
2025-12-14 03:10:52 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <string.h>
-#include "../headers/histo_handler.h" 
-
+#include <stdlib.h>
+#include "../headers/histo_handler.h"
+#include "../headers/leaks_handler.h" 
 
 int main(int argc, char *argv[]) {
-    // 1. Vérification minimale
+    // Vérification arguments
     if (argc < 2) {
         fprintf(stderr, "Usage: %s <commande> [args...]\n", argv[0]);
         return 1;
@@ -12,10 +13,8 @@ int main(int argc, char *argv[]) {
 
     const char *command = argv[1];
 
-    // 2. Aiguillage vers HISTO
+    //HISTO
     if (strcmp(command, "histo") == 0) {
-        // Le Shell envoie : ./wildwater histo <mode> <fichier>
-        // argv[0]=prog, argv[1]=histo, argv[2]=mode, argv[3]=fichier
         if (argc < 4) {
             fprintf(stderr, "Erreur: Arguments manquants pour histo (mode ou fichier).\n");
             return 1;
@@ -25,11 +24,25 @@ int main(int argc, char *argv[]) {
 
         return handle_histo_data(mode, filename);
     } 
-    // 3. Aiguillage vers LEAKS (Structure vide pour l'instant)
+    
+    //LEAKS
     else if (strcmp(command, "leaks") == 0) {
-        printf("Mode 'leaks' en cours de construction.\n");
-        return 0; 
+        
+        if (argc < 4) {
+            fprintf(stderr, "Erreur: Arguments manquants pour leaks (ID usine ou fichier).\n");
+            return 1;
+        }
+        const char *facility_id = argv[2];
+        const char *filename = argv[3];
+
+       
+        int ret = handle_leaks_data(facility_id, filename);
+        
+        
+        return ret;
     }
+    
+    
     else {
         fprintf(stderr, "Erreur: Commande '%s' inconnue.\n", command);
         return 1;
