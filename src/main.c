7ffb2511a28088d7bd1ed4f736126f6b714a3896
Ilyes1810 <mdk95130@gmail.com>
2025-12-15 15:55:53 +0100

    Refactor command handling in main.c

--- src/main.c
+++ src/main.c
@@ -10,9 +10,8 @@ int main(int argc, char *argv[]) {
         fprintf(stderr, "Usage: %s <commande> [args...]\n", argv[0]);
         return 1;
     }
-
+    
     const char *command = argv[1];
-
     //HISTO
     if (strcmp(command, "histo") == 0) {
         if (argc < 4) {
@@ -21,7 +20,6 @@ int main(int argc, char *argv[]) {
         }
         const char *mode = argv[2];
         const char *filename = argv[3];
-
         return handle_histo_data(mode, filename);
     } 
     
@@ -34,19 +32,12 @@ int main(int argc, char *argv[]) {
         }
         const char *facility_id = argv[2];
         const char *filename = argv[3];
-
-       
         int ret = handle_leaks_data(facility_id, filename);
-        
-        
         return ret;
     }
-    
-    
     else {
         fprintf(stderr, "Erreur: Commande '%s' inconnue.\n", command);
         return 1;
     }
-
     return 0;
 }
