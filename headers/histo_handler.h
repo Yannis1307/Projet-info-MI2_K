5b65879a0c58fba163216eb2b06b7cfadb43be90
Ilyes1810 <mdk95130@gmail.com>
2025-12-15 15:48:14 +0100

    Refactor histo_handler.h includes and prototypes

--- headers/histo_handler.h
+++ headers/histo_handler.h
@@ -4,13 +4,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "types.h"
-#include "avl_usine.h" // Nécessaire pour utiliser les fonctions AVL
+#include "avl_usine.h" 
 
-// Prototype de la fonction principale de lecture/calcul
-// Prend en entrée le mode d'histogramme (ex: "max", "src", "real")
-int handle_histo_data(const char *mode, const char *data_filename);// remplace datafilename par stdin pour le shell 
-
-// Prototype de la fonction d'écriture (à implémenter plus tard)
+int handle_histo_data(const char *mode, const char *data_filename);
 int write_histo_results(AVL_Node_Usine_t *root, const char *mode);
 
-#endif // HISTO_HANDLER_H
+#endif 
