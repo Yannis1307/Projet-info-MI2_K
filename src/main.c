1318fb87e555b5c1ffd0f0908712728022950d44
Yannis1307 <mistergameryl@gmail.com>
2025-12-19 16:52:43 +0100

    Change command and argument types to char*
    Updated variable types for command, mode, filename, and facility_id to char* for better compatibility with string manipulation.

--- src/main.c
+++ src/main.c
@@ -5,36 +5,37 @@
 #include "../headers/leaks_handler.h" 
 
 int main(int argc, char *argv[]) {
-    // Basic Argument Check
+    //validate minimum argument count 
     if (argc < 2) {
         fprintf(stderr, "Usage: %s <command> [args...]\n", argv[0]);
         return 1;
     }
     
-    const char *command = argv[1];
+    char *command = argv[1];
     
-    // HISTO
+    // process "histo" command: check the args and call specific handler
     if (strcmp(command, "histo") == 0) {
         if (argc < 4) {
             fprintf(stderr, "Error: Missing arguments for histo (mode or file).\n");
             return 1;
         }
-        const char *mode = argv[2];
-        const char *filename = argv[3];
+        char *mode = argv[2];
+        char *filename = argv[3];
         return handle_histo_data(mode, filename);
     } 
     
-    // LEAKS
+    //process "leaks" command:check args and call specific handler 
     else if (strcmp(command, "leaks") == 0) {
         if (argc < 4) {
             fprintf(stderr, "Error: Missing arguments for leaks (Factory ID or file).\n");
             return 1;
         }
-        const char *facility_id = argv[2];
-        const char *filename = argv[3];
+        char *facility_id = argv[2];
+        char *filename = argv[3];
         int ret = handle_leaks_data(facility_id, filename);
         return ret;
     }
+        //handle unknown commands 
     else {
         fprintf(stderr, "Error: Unknown command '%s'.\n", command);
         return 1;
