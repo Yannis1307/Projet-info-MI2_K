19d810eea78bbb9df80a1e80de8573a82ca93a43
Ilyes1810 <mdk95130@gmail.com>
2025-12-19 16:55:35 +0100

    Refactor CSV parser functions and enhance comments
    Refactor CSV parsing logic and improve comments for clarity.

--- Histo/csv_parser.c
+++ Histo/csv_parser.c
@@ -1,5 +1,5 @@
 #include "../headers/csv_parser.h"
-
+// Analyzes the beginning of the 'id' string to check if it matches one of the four production prefixes returns 1 if found, and 0 otherwise.
 int is_plant_prefix_valid(char *id) {
     if (strstr(id, "Plant ") == id) return 1;
     if (strstr(id, "Module ") == id) return 1;
@@ -8,9 +8,11 @@ int is_plant_prefix_valid(char *id) {
     return 0;
 }
 
+//If the data is present, it converts the text into a real number using the atof function.
+
 float parse_float(char *token) {
     if (token == NULL || strcmp(token, "-") == 0) {
-        return -1.0f; // value for missing
+        return -1.0f; 
     }
     return (float)atof(token);
 }
@@ -19,7 +21,7 @@ int parse_csv_line(char *line, CSV_Section_t *section) {
     char line_copy[MAX_LINE_LENGTH];
     char *token;
     int col_index = 0;
-    
+    //préparation and cleaning
     if (line == NULL || strlen(line) >= MAX_LINE_LENGTH || strlen(line) == 0) {
         return -1; 
     }
@@ -33,27 +35,27 @@ int parse_csv_line(char *line, CSV_Section_t *section) {
     line_copy[strcspn(line_copy, "\r\n")] = '\0';
     
     token = strtok(line_copy, FIELD_DELIMITER);
-    
+    //Iterates through each column one by one to store the data in the correct fields
     while (token != NULL) {
         while (*token == ' ') token++; 
         
         switch (col_index) {
-            case 0: // Col 1: Plant ID
+            case 0: 
                 if (strcmp(token, "-") != 0) 
                     strncpy(section->plant_id, token, MAX_ID_LEN - 1);
                 break;
-            case 1: // Col 2: Upstream ID
+            case 1: 
                 if (strcmp(token, "-") != 0) 
                     strncpy(section->upstream_id, token, MAX_ID_LEN - 1);
                 break;
-            case 2: // Col 3: Downstream ID
+            case 2: 
                 if (strcmp(token, "-") != 0) 
                     strncpy(section->downstream_id, token, MAX_ID_LEN - 1);
                 break;
-            case 3: // Col 4: Volume/Capacity
+            case 3: 
                 section->volume_or_capacity = parse_float(token);
                 break;
-            case 4: // Col 5: Leak %
+            case 4: 
                 section->leak_percentage = parse_float(token);
                 break;
             default:
@@ -62,14 +64,14 @@ int parse_csv_line(char *line, CSV_Section_t *section) {
         col_index++;
         token = strtok(NULL, FIELD_DELIMITER);
     }
-    
+    //This part of the function serves to secure missing data and to categorize the line to determine if it is a plant or an intake section.
     if (col_index <= 4) section->leak_percentage = -1.0f;
     if (col_index <= 3) section->volume_or_capacity = -1.0f;
     if (col_index <= 2) section->downstream_id[0] = '\0';
     
     if (section->volume_or_capacity != -1.0f && section->downstream_id[0] == '\0' && section->leak_percentage == -1.0f) {
         if (is_plant_prefix_valid(section->upstream_id)) {
-            // It's a Plant 
+             
             section->type = TYPE_PLANT;
         } else {
             section->type = TYPE_UNKNOWN; 
